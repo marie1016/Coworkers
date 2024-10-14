7d3e5b5ab159fcb95abd93778169d7aa1686da6a
Jaeheon96 <jinnavsi015@naver.com>
2024-10-14 15:02:18 +0900

    feat: 로그인 프로토타입 구현

--- src/core/api/axiosInstance.ts
+++ src/core/api/axiosInstance.ts
@@ -4,4 +4,12 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const reqConfig = config;
+  const accessToken = localStorage.getItem("accessToken");
+  reqConfig.headers.Authorization = `bearer ${accessToken}`;
+
+  return config;
+});
+
 export default axiosInstance;
