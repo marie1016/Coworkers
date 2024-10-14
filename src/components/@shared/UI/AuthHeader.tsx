4fceb16061195e3ebb1329d93b8883bb11f90a79
kzlsk <rlaflsk4887@gmail.com>
2024-10-15 04:23:31 +0900

    rename : UnAuthHeader -> SetupHeader로 파일명 변경

---
+++ src/components/@shared/UI/AuthHeader.tsx
@@ -0,0 +1,3 @@
+export default function AuthHeader() {
+  return <h1>로그인용 헤더</h1>;
+}
--- src/components/@shared/UI/SetupHeader.tsx
+++ src/components/@shared/UI/SetupHeader.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function UnAuthHeader() {
+export default function SetupHeader() {
   return (
     <header className="z-99 border-b-solid fixed left-0 right-0 top-0 flex h-[60px] items-center justify-between gap-2.5 border-border-primary bg-background-secondary px-4 py-5 lg:px-[360px] lg:py-3.5">
       <Link href="/">
