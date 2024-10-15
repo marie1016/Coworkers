368f1355ffeb5696302817c0acf8f330a9676158
Jaeheon96 <jinnavsi015@naver.com>
2024-10-15 18:51:42 +0900

    feat: 팀 추가 UI 1차 구현-공통 컴포넌트 미적용

--- src/pages/addteam/index.tsx
+++ src/pages/addteam/index.tsx
@@ -1,19 +1,21 @@
 import FileInput from "@/components/@shared/UI/FileInput";
+import InputLabel from "@/components/@shared/UI/InputLabel";
+import ProfileImagePreview from "@/components/@shared/UI/ProfileImagePreview";
 import addTeam from "@/core/api/group/addTeam";
 import { AddTeamResponse } from "@/core/dtos/group/group";
 import useImageUpload from "@/lib/hooks/useImageUpload";
 import { AxiosResponse } from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Addteam() {
   const {
     fileInputValue,
     file,
     handleFileInputChange,
-    clearFileInput,
     getImageUrl,
+    clearFileInput,
     imagePreview,
   } = useImageUpload();
   const [teamName, setTeamName] = useState("");
@@ -22,11 +24,6 @@ export default function Addteam() {
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTeamName(e.target.value);
-  };
-
-  const handlePreviewClear = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    clearFileInput();
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -44,22 +41,49 @@ export default function Addteam() {
   };
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit}>
-      <FileInput value={fileInputValue} onInputChange={handleFileInputChange}>
-        이미지
-      </FileInput>
-      {imagePreview ? (
-        <Image src={imagePreview} width={64} height={64} alt="이미지 프리뷰" />
-      ) : null}
-      <button type="button" onClick={handlePreviewClear}>
-        프리뷰 지우기
-      </button>
-      <input
-        className="text-black"
-        value={teamName}
-        onChange={handleNameChange}
-      />
-      <button type="submit">생성하기</button>
+    <form
+      className="mx-auto mt-52 flex max-w-[30.75rem] flex-col items-center gap-10 px-4"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex w-full flex-col items-center gap-20 sm:gap-6">
+        <h2 className="text-4xl font-medium text-text-primary sm:text-2xl md:text-2xl">
+          팀 생성하기
+        </h2>
+        <div className="flex w-full flex-col gap-6">
+          <InputLabel label="팀 프로필">
+            <FileInput
+              value={fileInputValue}
+              onInputChange={handleFileInputChange}
+            >
+              <ProfileImagePreview
+                preview={imagePreview}
+                clearFile={clearFileInput}
+              >
+                <div className="relative h-6 w-6">
+                  <Image
+                    fill
+                    src="/icons/icon-imagePlaceholder.svg"
+                    alt="이미지"
+                  />
+                </div>
+              </ProfileImagePreview>
+            </FileInput>
+          </InputLabel>
+          <InputLabel label="팀 이름">
+            <input
+              className="text-black"
+              value={teamName}
+              onChange={handleNameChange}
+            />
+          </InputLabel>
+        </div>
+      </div>
+      <div className="itmes-center flex flex-col gap-6">
+        <button type="submit">생성하기</button>
+        <p className="text-text-lg font-regular text-text-primary sm:text-text-md">
+          팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
+        </p>
+      </div>
     </form>
   );
 }
