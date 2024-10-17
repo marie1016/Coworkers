296069c9d8470a7b5df2a2ece710c35b216d7e50
kzlsk <rlaflsk4887@gmail.com>
2024-10-17 23:11:24 +0900

    refactor : input 수정

--- src/components/@shared/UI/Input.tsx
+++ src/components/@shared/UI/Input.tsx
@@ -1,45 +1,32 @@
-import { ReactNode } from "react";
+import { InputHTMLAttributes, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface InputProps {
-  type?: string; // 입력 필드 타입
-  placeholder?: string; // placeholder
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   buttonContent?: ReactNode; // 버튼에 들어갈 이지미, 텍스트
   errorMessage?: string; // 에러 메시지
   isValid?: boolean; // 유효성 검사
-  inputClassName?: string; // input 스타일
   buttonClassName?: string; // 버튼 스타일
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // 입력 값 변경 핸들러
   onButtonClick?: () => void; // 버튼 클릭 핸들러
 }
 
 export default function Input({
-  type = "text",
-  placeholder = "",
   buttonContent = null,
   errorMessage = "",
   isValid = true,
-  inputClassName = "",
   buttonClassName = "",
-  value = "",
-  onChange,
   onButtonClick,
+  className,
+  ...props
 }: InputProps) {
   const inputClasses = twMerge(
-    `h-[48px] w-[460px] rounded-xl border-border-primary border-opacity-10 bg-background-secondary p-4 placeholder:text-text-default hover:border-interaction-hover focus:border-interaction-hover focus:outline-none focus:ring-0 ${isValid ? "border-border-primary" : "border-status-danger focus:border-status-danger"}`,
-    inputClassName,
+    `h-[48px] w-[460px] rounded-xl border-border-primary border-opacity-10 bg-background-secondary p-4 placeholder:text-text-default hover:border-interaction-hover focus:border-interaction-hover focus:outline-none focus:ring-0 
+    ${isValid ? "border-border-primary" : "border-status-danger focus:border-status-danger"}`,
+    className,
   );
 
   return (
     <div className="relative flex flex-col gap-1">
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className={inputClasses}
-      />
+      <input {...props} className={inputClasses} />
       {buttonContent && (
         <button
           type="button"
@@ -57,14 +44,9 @@ export default function Input({
 }
 
 Input.defaultProps = {
-  type: "text",
-  placeholder: "",
   buttonContent: null,
   errorMessage: "",
   isValid: true,
-  inputClassName: "",
   buttonClassName: "",
-  value: "",
-  onChange: () => {},
   onButtonClick: () => {},
 };
