67e901e2f04324503d65a048ad1b95b5b5653dfc
Jaeheon96 <jinnavsi015@naver.com>
2024-10-27 00:34:53 +0900

    refactor: 모달 상태가 useModalStore를 사용하도록 리팩토링

--- src/components/PageComponents/team/MemberMenu.tsx
+++ src/components/PageComponents/team/MemberMenu.tsx
@@ -1,27 +1,24 @@
 import Dropdown from "@/components/@shared/UI/Dropdown";
 import DropdownItem from "@/components/@shared/UI/Item";
 import Image from "next/image";
-import { useState } from "react";
+import useModalStore from "@/lib/hooks/stores/modalStore";
 import MemberProfileModal from "./MemberProfileModal";
-import DeleteMemberModal from "./DeleteMemberModal";
 
 interface Props {
-  teamId: string;
-  memberId: string;
   image: string;
   name: string;
   email: string;
 }
 
-export default function MemberMenu({
-  teamId,
-  memberId,
-  image,
-  name,
-  email,
-}: Props) {
-  const [isProfileOpen, setIsProfileOpen] = useState(false);
-  const [isMemberDeleteModalOpen, setIsMemberDeleteModalOpen] = useState(false);
+export default function MemberMenu({ image, name, email }: Props) {
+  const profileModalName = "profileModal";
+
+  const isProfileOpen = useModalStore(
+    (state) => state.modals[profileModalName],
+  );
+
+  const openModal = useModalStore((state) => state.openModal);
+  const closeModal = useModalStore((state) => state.closeModal);
 
   return (
     <>
@@ -34,7 +31,7 @@ export default function MemberMenu({
         menuClassName="flex flex-col text-text-primary font-regular text-text-md w-[7.5rem] bg-background-secondary border border-solid border-border-primary right-0 top-6"
       >
         <DropdownItem
-          onClick={() => setIsProfileOpen(!isProfileOpen)}
+          onClick={() => openModal(profileModalName)}
           itemClassName="h-10 flex justify-center items-center"
         >
           프로필 보기
@@ -42,16 +39,10 @@ export default function MemberMenu({
       </Dropdown>
       <MemberProfileModal
         isOpen={isProfileOpen}
-        onClose={() => setIsProfileOpen(false)}
+        onClose={() => closeModal(profileModalName)}
         image={image}
         name={name}
         email={email}
-      />
-      <DeleteMemberModal
-        isOpen={isMemberDeleteModalOpen}
-        onClose={() => setIsMemberDeleteModalOpen(false)}
-        teamId={teamId}
-        memberId={memberId}
       />
     </>
   );
--- src/components/PageComponents/team/Members.tsx
+++ src/components/PageComponents/team/Members.tsx
@@ -37,8 +37,6 @@ export default function Members({ members }: Props) {
               </div>
             </div>
             <MemberMenu
-              teamId={`${member.groupId}`}
-              memberId={`${member.userId}`}
               image={member.userImage}
               name={member.userName}
               email={member.userEmail}
