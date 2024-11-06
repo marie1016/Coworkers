45956e304914ab3c912ec551a64526441562c286
kjw0412c <kjw0412c@gmail.com>
2024-11-06 19:35:38 +0900

    refactor: 랜딩 페이지 위치 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,95 +1,15 @@
-import axiosInstance from "@/core/api/axiosInstance";
-import { AxiosResponse } from "axios";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-
-interface UserBase {
-  id: number;
-  teamId: string;
-  email: string;
-  nickname: string;
-  image: string | null;
-  createdAt: string;
-  updatedAt: string;
-}
-
-interface Membership {
-  group: {
-    id: number;
-    teamId: string;
-    name: string;
-    image: string;
-    createdAt: string;
-    updatedAt: string;
-  };
-  userId: number;
-  groupId: number;
-  role: "ADMIN" | "MEMBER";
-  userName: string;
-  userImage: string;
-  userEmail: string;
-}
-
-interface UserResponse extends UserBase {
-  memberships: Membership[];
-}
-
-const INITIAL_USER_INFO: UserResponse = {
-  id: 0,
-  teamId: "",
-  email: "",
-  nickname: "",
-  image: null,
-  createdAt: "",
-  updatedAt: "",
-  memberships: [],
-};
+import Main from '@/components/PageComponents/home/Main';
+import Content from '@/components/PageComponents/home/Content';
+import Footer from '@/components/PageComponents/home/Footer';
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useState(INITIAL_USER_INFO);
-
-  const getUserInfo = async () => {
-    let res: AxiosResponse<UserResponse>;
-    try {
-      res = await axiosInstance.get("user");
-    } catch (error) {
-      console.error(error);
-      return;
-    }
-    setUserInfo(res.data);
-  };
-
-  useEffect(() => {
-    getUserInfo();
-  }, []);
-
   return (
-    <div>
-      <Link href="/signup">회원가입 페이지</Link>
-      <br />
-      <Link href="/login">로그인 페이지</Link>
-      <br />
-      <Link href="/addteam">팀 생성하기 페이지</Link>
-      <br />
-      <Link href="/mypage">마이페이지</Link>
-      <br />
-      <Link href="/boards">자유게시판</Link>
-      <br />
-      <Link href="/addboard">게시물 등록 페이지</Link>
-      <br />
-      <Link href="/landingpage">랜딩 페이지</Link>
-      <br />
-      <br />
-      <h2 className="text-text-3xl">내가 속한 팀</h2>
-      <br />
-      {userInfo.memberships.map((membership) => (
-        <>
-          <Link href={`/${membership.groupId}`} key={membership.groupId}>
-            {membership.group.name}
-          </Link>
-          <br />
-        </>
-      ))}
-    </div>
+    <>
+      <main>
+        <Main />
+        <Content />
+      </main>
+      <Footer />
+    </>
   );
 }
