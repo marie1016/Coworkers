9421e0e95c0582779ac309e43ff8bb2334e9ba54
kzlsk <rlaflsk4887@gmail.com>
2024-11-07 15:47:51 +0900

    refactor : 헤더 수정

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,3 @@
-import AuthHeader from "@/components/@shared/UI/AuthHeader";
-
 export default function Signup() {
-  return (
-    <div>
-      <AuthHeader />
-      <div>회원가입 페이지</div>;
-    </div>
-  );
+  return <div>회원가입 페이지</div>;
 }
